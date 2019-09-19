19b3b5468c76acc543aaf538c99703e6a8679956
Areli-vazquez <aremonvazsalgmail.com>
2019-09-19 17:32:27 -0500

    Se agrego del problema1_1 al problema1_4

---
+++ LIBRO/problemas_resueltos/capitulo1/problema1_1.py
@@ -0,0 +1,5 @@
+PREPRO = float(input("Precio del producto:"))
+PAGO =float(input("¿De cuanto es tù pago?"))
+DEVO = PAGO - PREPRO 
+print(f"Pagaste ${PAGO} y el precio del producto es: ${PREPRO} y tu cambio es ${DEVO}")
+
---
+++ LIBRO/problemas_resueltos/capitulo1/prolema1_2.py
@@ -0,0 +1,7 @@
+BASE =  float(input("Dame la base del triangulo:"))
+ALTU = float(input("Dame la altura del triangulo:"))
+SUP = (BASE*ALTU)/2
+print(f"La superficie del triangulo es {SUP} , dado que la altura es {ALTU} u. y la base es de {BASE} u. "
+
+        )
+
