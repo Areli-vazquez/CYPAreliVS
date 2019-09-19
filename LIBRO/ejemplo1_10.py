438140d6d0999bf788891713776413cb6260c7a2
Areli-vazquez <aremonvazsal@gmail.com>
2019-09-18 21:35:48 -0500

    Problema1_10 del libro

---
+++ LIBRO/ejemplo1_10.py
@@ -0,0 +1,7 @@
+RADIO = int(input("Dame un número real:"))
+ALTU = int(input("Dame un número real:"))
+VOL = 3.141592 * (RADIO**2) * ALTU
+ARE = 2* 3.141592 * RADIO * ALTU
+print(f"El {VOL} del cilindro")
+print(f"El {ARE} del cilindro")
+
