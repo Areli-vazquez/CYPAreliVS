4cf489e8c48101f9f664dd6c2c61f3a0f085d1c3
Areli-vazquez <aremonvazsal@gmail.com>
2019-09-24 01:26:02 -0500

    Se agrega problema1_10 del libro

--- LIBRO/ejemplo1_10.py
+++ LIBRO/ejemplo1_10.py
@@ -2,7 +2,7 @@ BASE = float(input("ingresa la medida de la base:"))
 ALTU = float(input("ingresa la medida de la altura:"))
 SUP = BASE * ALTU
 PER = 2*(BASE + ALTU)
-print(f"La superficie del rectangulo es: {SUP}")
-print(f"El perimetro del rectangulo es: {PER} ")
+print(f"El area del rectangulo es: {SUP}cm ")
+print(f"El perimetro del rectangulo es: {PER}cm ")
 
 
