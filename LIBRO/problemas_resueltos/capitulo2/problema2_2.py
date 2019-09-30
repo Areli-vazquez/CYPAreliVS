ea929a042aed7b4cb2d001a6f5511797ae1e69db
Areli-vazquez <aremonvazsal@gmail.com>
2019-09-29 22:42:58 -0500

    Se agrega problema2_2 del libro

---
+++ LIBRO/problemas_resueltos/capitulo2/problema2_2.py
@@ -0,0 +1,7 @@
+P = int(input("Dame un número entero:"))
+Q = int(input("Dame un segundo número entero:"))
+EXP = P**3 + Q**4-2*P**2
+if EXP < 680:
+    print(f"Cumplen con la expresión y sus valores son: {P} y {Q}")
+print("Fin del programa")
+
