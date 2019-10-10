5da31a6adce4c94e65572a3bb278b90490bf8325
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-10 01:48:35 -0500

    Se agrega problema2_5 del libro

---
+++ LIBRO/problemas_resueltos/capitulo2/problema2_5.py
@@ -0,0 +1,9 @@
+NUM = int(input("Escribe un número:"))
+if NUM > 0:
+    print(f"El número es POSITIVO")
+else:
+    if NUM == 0:
+        print(f"El número es NULO")
+    else:
+        print(f"El número es NEGATIVO")
+    print("FIN DEL PROGRAMA")
