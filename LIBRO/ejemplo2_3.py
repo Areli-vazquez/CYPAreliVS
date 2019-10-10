5a867abff62739982d5533f30488dfb477ef0176
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-10 01:14:38 -0500

    Se agrega el ejemplo2_3 del libro

---
+++ LIBRO/ejemplo2_3.py
@@ -0,0 +1,7 @@
+NOM = str(input("Escribe tu nombre completo:"))
+CAL = float(input("Ingresa tu calificación:"))
+if CAL >= 8:
+    print(f"El alumno {NOM} esta APROBADO")
+else:
+    print(f"El alumno {NOM} esta REPROBADO")
+print("FIN DEL PROGRAMA")
