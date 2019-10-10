78d267cc85eff86cc02e3b8cf47c16aaa0d8c96c
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-10 01:47:18 -0500

    .

--- LIBRO/problemas_resueltos/problema2_5.py
+++
@@ -1,9 +0,0 @@
-NUM = int(input("Escribe un número:"))
-if NUM > 0:
-    print(f"El número es POSITIVO")
-else:
-    if NUM == 0:
-        print(f"El número es NULO")
-    else:
-        print(f"El número es NEGATIVO")
-    print("FIN DEL PROGRAMA")
