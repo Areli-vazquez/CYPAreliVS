a52009901cecc18fd2545b2ee846eabf98e5b79f
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-10 01:59:36 -0500

    Se agrega ejemplo2_12.py del libro

--- LIBRO/ejemplo2_12.py
+++ LIBRO/ejemplo2_12.py
@@ -4,18 +4,20 @@ C = int(input("Dame un numero entero positivo:"))
 
 if A > B:
     if A > C:
-        if B > C: 
-        print(f"{A} > {B} > {C}")
+        if B > C:
+            print(f"{A} > {B} > {C}")
+        else:
+            print(f"{A} > {C} > {B}")
     else:
-        print(f"{A} > {C} > {B}")
-  else: 
         print(f"{C} > {A} > {B}")
+
 else:
     if B > C:
         if A > C:
             print(f"{B} > {A} > {C}")
-    else:
-            print(f"{C} > {B} > {A}")
-  else:
+        else: 
             print(f"{B} > {C} > {A}")
-        print("FIN DEL PROGRAMA")
+    else:
+        print(f"{C} > {B} > {A}")
+
+print("Fin del programa")
