977b0a9371cfe123eb0d2e49a722de7264d3d659
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-12 00:33:33 -0500

    Se agrega problema 2_13 del libro

---
+++ LIBRO/problemas_resueltos/capitulo2/problema2_13.py
@@ -0,0 +1,19 @@
+NOM = str(input("Escribe tu nombre completo:"))
+MAT = int(input("Escribe tu matricula de alumno:"))
+CARR = str(input("El alumno está inscrito en la carrera de:"))
+SEM = int(input("El alumno está cursando el semestre:"))
+PROM = float(input("El alumno tiene un promedio de:"))
+if CARR =="Economia":
+    SEM >=6
+    PROM >=8.8
+elif CARR =="Computación":
+    SEM >6 
+    PROM >8.5
+elif CARR =="Contabilidad": 
+        SEM > 5
+        PROM > 8.5
+elif CARR =="Administración":
+        SEM > 5
+        PROM > 8.5
+        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
+print("FIN DEL PROGRAMA")
