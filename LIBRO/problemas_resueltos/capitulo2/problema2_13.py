d251bb6cbe87ceae94db8898224d28f20dcfecab
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-12 01:49:55 -0500

    Se agrega el problema2_13 del libro

--- LIBRO/problemas_resueltos/capitulo2/problema2_13.py
+++ LIBRO/problemas_resueltos/capitulo2/problema2_13.py
@@ -5,15 +5,29 @@ SEM = int(input("El alumno está cursando el semestre:"))
 PROM = float(input("El alumno tiene un promedio de:"))
 if CARR =="Economia":
     SEM >=6
-    PROM >=8.8
+    if PROM >=8.8:
+        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
+    else:
+        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} no a sido ACEPTADO")
+        
 elif CARR =="Computación":
     SEM >6 
-    PROM >8.5
+    if PROM >=8.5:
+        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
+    else:
+        print(f"El alumno {NOM}  no a sido ACEPTADO")
+        
 elif CARR =="Contabilidad": 
         SEM > 5
-        PROM > 8.5
+        if PROM >= 8.5:
+             print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
+        else:
+            print(f"El alumno {NOM}  no a sido ACEPTADO")
+            
 elif CARR =="Administración":
         SEM > 5
-        PROM > 8.5
-        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
+        if PROM >= 8.5:
+             print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
+        else:
+            print(f"El alumno {NOM}  no a sido ACEPTADO")
 print("FIN DEL PROGRAMA")
