51b663fb7087acf87a1bc86dfeaac0a4b04e870c
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-13 00:16:33 -0500

    Se agrega problema2_13 del libro

--- LIBRO/problemas_resueltos/capitulo2/problema2_13.py
+++ LIBRO/problemas_resueltos/capitulo2/problema2_13.py
@@ -5,29 +5,18 @@ SEM = int(input("El alumno está cursando el semestre:"))
 PROM = float(input("El alumno tiene un promedio de:"))
 if CARR =="Economia":
     SEM >=6
-    if PROM >=8.8:
-        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
-    else:
-        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} no a sido ACEPTADO")
-        
+    PROM >=8.8
+    print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
 elif CARR =="Computación":
     SEM >6 
-    if PROM >=8.5:
-        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
-    else:
-        print(f"El alumno {NOM}  no a sido ACEPTADO")
-        
+    PROM >=8.5
+    print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")        
 elif CARR =="Contabilidad": 
         SEM > 5
-        if PROM >= 8.5:
-             print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
-        else:
-            print(f"El alumno {NOM}  no a sido ACEPTADO")
-            
+        PROM >= 8.5
+        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")            
 elif CARR =="Administración":
         SEM > 5
-        if PROM >= 8.5:
-             print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
-        else:
-            print(f"El alumno {NOM}  no a sido ACEPTADO")
+        PROM >= 8.5
+        print(f"El alumno {NOM} con la matrícula {MAT} de la carrera de {CARR} a sido ACEPTADO")
 print("FIN DEL PROGRAMA")
