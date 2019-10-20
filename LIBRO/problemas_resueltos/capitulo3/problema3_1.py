2a4ee9f34abe114784613ac1bec33a9503369c24
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-19 20:15:03 -0500

    Se agrega el problema 3_1 del libro

---
+++ LIBRO/problemas_resueltos/capitulo3/problema3_1.py
@@ -0,0 +1,23 @@
+SUMPAR = 0
+SUMIMP = 0
+CUEPAR = 0 
+PROPAR = 0
+I = 1
+for I in range(1, 270, 10):
+    NUM = int(input("Ingresa un número entero"))
+    while I <= 270:
+        if (-1 ** NUM) > 0:
+            SUMPAR += NUM
+            CUEPAR += 1
+            print("El promedio total es: PROPAR")
+        else: 
+            if ( -1 ** NUM ) <0:
+                SUMIMP += NUM
+                print("La suma impar es:", SUMIMP)
+print( "FIN DEL PROGRAMA")
+
+    
+            
+
+
+           
