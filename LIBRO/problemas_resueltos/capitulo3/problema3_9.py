4e3769dc7acd83a09731e30daa4c997516fb253e
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-24 03:39:15 -0500

    Se agrega el problema3_9 del libro

---
+++ LIBRO/problemas_resueltos/capitulo3/problema3_9.py
@@ -0,0 +1,8 @@
+SERIE = 0
+N = int(input("Ingresa el número de terminos que tendrá la serie:"))
+I = 1
+for I in range(1, N, 1):
+    SERIE = SERIE + I**I
+    I +=1
+    print("La serie es:", SERIE)
+print("FIN DEL PROGRAMA")
