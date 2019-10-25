41488ee992a134fcc832666bcdfc7a10018d8add
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-25 04:11:24 -0500

    Se agrega problema3_8 del libro

--- LIBRO/problemas_resueltos/capitulo3/problema3_8.py
+++ LIBRO/problemas_resueltos/capitulo3/problema3_8.py
@@ -1,13 +1,15 @@
-NUM = bool(int(input("Ingresa un número:")))
+NUM =int(input("Ingresa un número:"))
 if NUM > 0: 
     while (NUM !=1):
         print("El numero es:", NUM)
         if (-1 ** NUM) >0: 
-            NUM = NUM /2
+            NUM /=2
         else:
              NUM = (NUM *3) + 1
-   
-print(NUM ,"TIENE QUE SER UN ENTERO POSITIVO")
+        print("El número es:", NUM)
+        NUM = int(input("Ingresa un número:"))
+else:
+    print(NUM ,"TIENE QUE SER UN ENTERO POSITIVO")
 print("FIN DEL PROGRAMA")
 
 
