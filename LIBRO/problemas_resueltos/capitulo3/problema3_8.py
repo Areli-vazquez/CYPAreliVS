3edadd76a3a3868bc0a3d436d20447ead3609491
Areli-vazquez <aremonvazsal@gmail.com>
2019-10-27 00:55:37 -0500

    se agrega el problema3_8 de ibro

--- LIBRO/problemas_resueltos/capitulo3/problema3_8.py
+++ LIBRO/problemas_resueltos/capitulo3/problema3_8.py
@@ -1,13 +1,11 @@
 NUM =int(input("Ingresa un número:"))
 if NUM > 0: 
     while (NUM !=1):
-        print("El numero es:", NUM)
-        if (-1 ** NUM) >0: 
+        if NUM % 2 == 0:
             NUM /=2
         else:
              NUM = (NUM *3) + 1
         print("El número es:", NUM)
-        NUM = int(input("Ingresa un número:"))
 else:
     print(NUM ,"TIENE QUE SER UN ENTERO POSITIVO")
 print("FIN DEL PROGRAMA")
